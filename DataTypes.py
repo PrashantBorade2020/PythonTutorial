aa9c97071927d5f7545a97da926bf989d79d7b5f
test123 <test@gmail.com>
2020-02-27 12:45:12 +0530

    int data type added

--- DataTypes.py
+++ DataTypes.py
@@ -1,7 +1,12 @@
 #Built in Data Types 
+print("Example : - ")
 #Text Type : str
 x = "Hello world"
 print(x)
 
-
+#Numeric Types : int , float , complex
+print("Example : - ")
+x=20
+print(x)
+print(type(x))
 
